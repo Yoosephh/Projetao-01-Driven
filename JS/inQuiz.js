b491c06a0e90932596864b492305538b617b8caa
Yoosephh <jose.es.oleg@gmail.com>
2023-04-19 10:36:37 -0300

    Jose: Primeiros passos para o inquiz

---
+++ JS/inQuiz.js
@@ -0,0 +1,6 @@
+// vai ser chamado quando o usuario clicar para iniciar um quiz
+
+// mostra as perguntas com 4 possiveis respostas. Precisa identificar o clique do usuario 
+//sinalizar se o clique foi numa resposta correta
+
+//ao final do quiz, identificar o desempenho do usuario e uma resposta qualquer para o desempenho
