b286fedf7aefcfd532156e45121bc8807dc097ed
THIAGO SOUSA <thiago21sousa21@gmail.com>
2023-04-19 09:25:34 -0300

    teste API

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,16 @@
+axios.defaults.headers.common['Authorization'] = 'aTbqbZrZrkTYLYdrTktudg1i';
+
+// thiago- testando funçao get:
+function respostaPromessaObterQuizes(res){
+    alert('promessa de obeter quizes cumprida');
+    console.log(res);
+}
+
+let promessaObterQuizes = axios.get('https://mock-api.driven.com.br/api/vm/buzzquizz/quizzes');
+promessaObterQuizes.then(respostaPromessaObterQuizes)
+// fim teste função get
+
+
 //Tela inicial do usuario
     //Cria um quiz
         //perguntas com input (imagem, endereco, resposta errada, certa....)
