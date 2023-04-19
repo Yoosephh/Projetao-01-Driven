7f606f117bfe5dbcdc4927ea7a81a0455f5f788c
THIAGO SOUSA <thiago21sousa21@gmail.com>
2023-04-19 10:31:55 -0300

    teste API 2

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,25 @@
 axios.defaults.headers.common['Authorization'] = 'aTbqbZrZrkTYLYdrTktudg1i';
+let dadosRecebidos;
 
 // thiago- testando funçao get:
 function respostaPromessaObterQuizes(res){
     alert('promessa de obeter quizes cumprida');
     console.log(res);
+    dadosRecebidos = res.data;
+    console.log(dadosRecebidos);
+    console.log(dadosRecebidos[0]);
+
+
+    const imagemQuizRecebido = document.querySelector('.quiz img');
+    console.log(imagemQuizRecebido);
+    imagemQuizRecebido.src = dadosRecebidos[0].image;
+
 }
 
 let promessaObterQuizes = axios.get('https://mock-api.driven.com.br/api/vm/buzzquizz/quizzes');
 promessaObterQuizes.then(respostaPromessaObterQuizes)
-// fim teste função get
+
+//thiago- fim teste função get
 
 
 //Tela inicial do usuario
@@ -16,6 +27,8 @@ promessaObterQuizes.then(respostaPromessaObterQuizes)
         //perguntas com input (imagem, endereco, resposta errada, certa....)
         //armazenar o quiz do usuario -> post
     //entrar em um quiz
+
+
         //logica para captar a resposta errada, e a resposta correta
         //Calcular o indice de acerto do usuario
     //
