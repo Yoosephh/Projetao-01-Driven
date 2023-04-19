bb15259a5701c3feb194a3fa1cecbb1520660683
Yoosephh <jose.es.oleg@gmail.com>
2023-04-18 22:57:02 -0300

    alteracoes no JS

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+//Tela inicial do usuario
+    //Cria um quiz
+        //perguntas com input (imagem, endereco, resposta errada, certa....)
+        //armazenar o quiz do usuario -> post
+    //entrar em um quiz
+        //logica para captar a resposta errada, e a resposta correta
+        //Calcular o indice de acerto do usuario
+    //
